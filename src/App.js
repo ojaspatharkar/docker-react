65cc45fdf753f5b2ae6f6e06ab175c8f4f3db607
Ojas Patharkar <patharkar.ojas@gmail.com>
2021-04-10 12:32:44 +0530

    text change

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hey there
+          Hola
         </p>
         <a
           className="App-link"
